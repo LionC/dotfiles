b7f671c47afac5dbe53b36fe81365cd3981a1a2e
Leon Strauss <me@lionc.de>
2021-08-14 15:21:44 +0200

    :recycle: Extract nest module

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,89 +1,8 @@
+local nest = require('nest')
+
 vim.g.mapleader = ' '
 
-local defaultConfig = {
-    mode = "n",
-    options = {
-        noremap = true,
-        silent = true,
-    },
-    prefix = "",
-}
-
-local function copy(table)
-    local ret = {}
-
-    for key, value in pairs(table) do
-        ret[key] = value
-    end
-
-    return ret
-end
-
-local function mergeTables(left, right)
-    local ret = copy(left)
-
-    for key, value in pairs(right) do
-        ret[key] = value
-    end
-
-    return ret
-end
-
-local function mergeOptions(left, right)
-    local ret = copy(left)
-
-    if (right == nil) then
-        return ret
-    end
-
-    if (right.mode ~= nil) then
-        ret.mode = right.mode
-    end
-
-    if (right.prefix ~= nil) then
-        ret.prefix = ret.prefix .. right.prefix
-    end
-
-    if (right.options ~= nil) then
-        ret.options = mergeTables(ret.options, right.options)
-    end
-
-    return ret
-end
-
-local function applyKeybinds (config, presets)
-    local presets = presets or defaultConfig
-    local mergedPresets = mergeOptions(presets, config)
-
-    local first = config[1]
-
-    if(type(first) == "table") then
-        for _, it in ipairs(config) do
-            applyKeybinds(it, mergedPresets)
-        end
-
-        return
-    end
-
-    local second = config[2]
-
-    mergedPresets.prefix = mergedPresets.prefix .. first
-
-    if(type(second) == "table") then
-        applyKeybinds(second, mergedPresets)
-
-        return
-    end
-
-    vim.api.nvim_set_keymap(
-        mergedPresets.mode,
-        mergedPresets.prefix,
-        second,
-        mergedPresets.options
-    )
-end
-
-applyKeybinds {
+nest.applyKeybinds {
     -- Pragmatic command mode
     { ";",      ":", options = { silent = false } },
     { ":",      ";" },
@@ -93,7 +12,7 @@ applyKeybinds {
     { "gb",     "<C-o>" },
 
     -- Searching
-    { "<Esc>",  ":nohl<CR>" },
+    { "<Esc>",  "<Cmd>nohl<CR>" },
     { "/",      "ms/" },
     { "?",      "ms?" },
 
@@ -111,11 +30,11 @@ applyKeybinds {
         { "H>", "<C-W>h" },
 
         -- Fast access Telescope
-        { "p>", ":Telescope find_files<CR>" },
-        { "f>", ":Telescope live_grep<CR>" },
+        { "p>", "<Cmd>Telescope find_files<CR>" },
+        { "f>", "<Cmd>Telescope live_grep<CR>" },
 
         -- File Tree
-        { "n>", ":NvimTreeToggle<CR>" },
+        { "n>", "<Cmd>NvimTreeToggle<CR>" },
     }},
 
     { "<leader>", {
@@ -130,8 +49,7 @@ applyKeybinds {
     }},
 
     { mode = "i", options = { expr = true },
-        { "<CR>",       "compe#confirm('<CR'>)" },
+        { "<CR>",       "compe#confirm('<CR>')" },
         { "<C-Space>",  "compe#complete()" },
     },
 }
-
