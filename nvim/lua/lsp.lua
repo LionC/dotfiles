3994c06215c1d51397f5f5adfd96481ef247afed
Leon Strauss <me@lionc.de>
2021-08-28 12:00:26 +0200

    :art:

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,48 +1,37 @@
 local nvim_lsp = require 'lspconfig'
+local nest = require 'nest'
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
   --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
+  nest.applyKeymaps {
+      buffer = true,
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+      { 'g', {
+          { 'd', '<Cmd>lua vim.lsp.buf.definition()<CR>' },
+          { 'y', '<cmd>lua vim.lsp.buf.type_definition()<CR>' },
+          { 'i', '<cmd>lua vim.lsp.buf.implementation()<CR>' },
+          { 'r', '<cmd>lua vim.lsp.buf.references()<CR>' },
+          { 'D', '<Cmd>lua vim.lsp.buf.declaration()<CR>' },
+      }},
 
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<C-E>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', '<C-e>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+      { 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>' },
+  }
 end
 
 -- or "tsserver"
 nvim_lsp.denols.setup { on_attach = on_attach }
 
-local sumneko_root_path = "/usr/local/opt/lua-language-server"
-local sumneko_binary = sumneko_root_path.."/bin/macOS/lua-language-server"
+local sumneko_root_path = '/usr/local/opt/lua-language-server'
+local sumneko_binary = sumneko_root_path..'/bin/macOS/lua-language-server'
 local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
 nvim_lsp.sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  cmd = {sumneko_binary, '-E', sumneko_root_path .. '/main.lua'};
   settings = {
     Lua = {
       runtime = {
@@ -53,7 +42,7 @@ nvim_lsp.sumneko_lua.setup {
         globals = { 'vim' },
       },
       workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
+        library = vim.api.nvim_get_runtime_file('', true),
       },
       telemetry = {
         enable = false,
