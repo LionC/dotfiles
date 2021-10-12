b65248982945028c4891e9466940d1673c532ebc
Leon Strauss <me@lionc.de>
2021-10-12 18:01:16 +0200

    :bug: Fix nvim setup

--- nvim/lua/plugins/config/nvim-tree.lua
+++ nvim/lua/plugins/config/nvim-tree.lua
@@ -41,15 +41,17 @@ return function ()
 
     require 'nvim-tree'.setup {
         -- closes neovim automatically when the tree is the last **WINDOW** in the view
-        auto_close          = true,
+        auto_close = true,
         -- hijack the cursor in the tree to put it at the start of the filename
-        hijack_cursor       = true,
+        hijack_cursor = true,
         -- show lsp diagnostics in the signcolumn
-        lsp_diagnostics     = true,
+        diagnostics = {
+            enable = true
+        },
         -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
         update_focused_file = {
           -- enables the feature
-          enable      = true,
+          enable = true,
         },
     }
 end
