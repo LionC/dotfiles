38574e25641b8673102598a480a180d11b871d97
Leon Strauss <me@lionc.de>
2023-02-14 17:58:26 +0100

    :wrench: Add turbo alias tu

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -4,4 +4,5 @@ alias txl="tmuxp load ."
 alias txa="tmux attach-session -t "
 alias gsl="git switch -"
 alias turbo="turbo --output-logs new-only"
+alias tu="pnpm turbo run --output-logs new-only"
 
