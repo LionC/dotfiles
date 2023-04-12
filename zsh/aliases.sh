628183c0b6ac19ab43a7ffd5bc76498f952ba601
Leon Strauss <me@lionc.de>
2023-04-12 12:21:32 +0200

    :sparkles: Add grocket alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -3,6 +3,7 @@ alias zshr="source $HOME/.zshrc"
 alias txl="tmuxp load ."
 alias txa="tmux attach-session -t "
 alias gsl="git switch -"
+alias grocket="git commit --allow-empty --message :rocket:"
 alias turbo="turbo --output-logs new-only"
 alias tu="pnpm turbo run --output-logs new-only"
 
