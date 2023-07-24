07fc1e1e8c45300b49b180eb4b0fa792c0fbbb06
Leon Strauss <me@lionc.de>
2023-07-24 14:47:29 +0200

    :wrench: Add concurrency to turbo alis

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -4,5 +4,5 @@ alias txl="tmuxp load ."
 alias txa="tmux attach-session -t "
 alias gsl="git switch -"
 alias grocket="git commit --allow-empty --message :rocket:"
-alias tu="pnpm turbo run --output-logs new-only"
+alias tu="pnpm turbo run --output-logs new-only --concurrency 15"
 
