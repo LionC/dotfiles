4d5c2c9a88c1d58867969b4f3c77f1f450e8c918
Leon Strauss <me@lionc.de>
2023-07-24 14:56:08 +0200

    :sparkles: Add exa alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,8 +1,15 @@
 alias nv="nvim"
 alias zshr="source $HOME/.zshrc"
+alias ls="exa --icons"
+
+#tmux
 alias txl="tmuxp load ."
 alias txa="tmux attach-session -t "
+
+# git
 alias gsl="git switch -"
 alias grocket="git commit --allow-empty --message :rocket:"
+
+# turbo
 alias tu="pnpm turbo run --output-logs new-only --concurrency 15"
 
