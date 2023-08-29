feee1d5db91091343c9035565b4cc29256bc8e21
Leon Strauss <me@lionc.de>
2023-08-29 22:47:09 +0200

    :lipstick: Add highlights for cmp

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,4 +1,12 @@
 vim.cmd 'colorscheme monokai_pro'
 
--- Overrides
-vim.cmd 'highlight NeoTreeTitleBar guibg=White guifg=Black'
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { link = "Identifier" })
+vim.api.nvim_set_hl(0, "CmpItemKindFunction", { link = "Function" })
+vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "Function" })
+vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "Type" })
+vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { link = "Keyword" })
+vim.api.nvim_set_hl(0, "CmpItemKindText", { link = "String" })
+
+--vim.cmd [[
+    --highlight link NeoTreeTitleBar NormalFloat
+--]]
