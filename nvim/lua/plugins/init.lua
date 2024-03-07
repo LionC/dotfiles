1129b8490248069f1aa099b4ad1d906bceab184c
Leon Strauss <me@lionc.de>
2024-03-07 02:54:15 +0100

    :heavy_plus_sign: Add nvim fidglet

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -34,6 +34,22 @@ require 'lazy'.setup {
         'lewis6991/gitsigns.nvim'
     }},
     -- { 'rcarriga/nvim-notify', config = require 'plugins.config.notify'  },
+    { 'j-hui/fidget.nvim', opts = {
+        progress = {
+            display = {
+                done_ttl = 1.2,
+                progress_style = "Comment",
+                group_style = "Comment",
+                icon_style = "Comment",
+                done_style = "Comment",
+            },
+        },
+        notification = {
+            window = {
+                winblend = 0,
+            },
+        },
+    }},
     { 'glepnir/dashboard-nvim', config = require 'plugins.config.dashboard', dependencies = {
         "nvim-tree/nvim-web-devicons",
         'lewis6991/gitsigns.nvim'
