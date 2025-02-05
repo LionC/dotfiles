450f61bb71037acd6fc5966ed3b7875555858a47
Leon Strauss <me@lionc.de>
2025-02-05 21:59:29 +0100

    :lipstick: Remove encoding and line endings from lualine config

--- nvim/lua/plugin-specs/lualine.lua
+++ nvim/lua/plugin-specs/lualine.lua
@@ -1,9 +1,13 @@
-return { 'nvim-lualine/lualine.nvim',
+return {
+    'nvim-lualine/lualine.nvim',
     dependencies = {
         'nvim-tree/nvim-web-devicons',
         'maxmx03/fluoromachine.nvim',
     },
     opts = {
         theme = 'fluoromachine',
+        sections = {
+            lualine_x = { "filetype" },
+        },
     },
 }
