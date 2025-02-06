32387f70672fbca2a2fe2a37de2e99a7fa5c45f3
Leon Strauss <me@lionc.de>
2025-02-06 18:50:26 +0100

    :art: Column align nvim options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -3,33 +3,33 @@ local homeFolder = vim.env.HOME
 vim.g.mapleader = ' '
 
 local options = {
-    autoread = true,
-    backup = false,
-    cmdheight = 1,
-    completeopt = { 'menuone', 'noselect' },
-    cursorline = true,
-    expandtab = true,
-    hidden = true,
-    ignorecase = true,
-    inccommand = 'nosplit',
-    lazyredraw = true,
-    mouse = 'a',
-    number = true,
+    autoread       = true,
+    backup         = false,
+    cmdheight      = 1,
+    completeopt    = { 'menuone', 'noselect' },
+    cursorline     = true,
+    expandtab      = true,
+    hidden         = true,
+    ignorecase     = true,
+    inccommand     = 'nosplit',
+    lazyredraw     = true,
+    mouse          = 'a',
+    number         = true,
     relativenumber = true,
-    scrolloff = 15,
-    shiftwidth = 4,
-    showmode = false,
-    signcolumn = 'yes',
-    smartcase = true,
-    tabstop = 4,
-    termguicolors = true,
-    timeoutlen = 600,
-    ttimeoutlen = 80,
-    undodir = homeFolder .. '/.vim/undofiles5',
-    undofile = true,
-    updatetime = 300,
-    wrap = false,
-    writebackup = false,
+    scrolloff      = 15,
+    shiftwidth     = 4,
+    showmode       = false,
+    signcolumn     = 'yes',
+    smartcase      = true,
+    tabstop        = 4,
+    termguicolors  = true,
+    timeoutlen     = 600,
+    ttimeoutlen    = 80,
+    undodir        = homeFolder .. '/.vim/undofiles5',
+    undofile       = true,
+    updatetime     = 300,
+    wrap           = false,
+    writebackup    = false,
 }
 
 for name, value in pairs(options) do
@@ -38,7 +38,7 @@ end
 
 vim.opt.shortmess:append { c = true }
 
-vim.g.loaded_netrw = 1
+vim.g.loaded_netrw       = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin   = 1
+vim.g.loaded_zip         = 1
